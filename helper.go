37c1bcca11a9847228f17801ec802eef9207a88d
mozillazg <mozillazg101@gmail.com>
2017-06-11 19:38:46 +0800

    add Bool, Int, String function

--- helper.go
+++ helper.go
@@ -10,27 +10,14 @@ func calMD5Digest(msg []byte) []byte {
 	return m.Sum(nil)
 }
 
-// contentMD5 computes and sets the HTTP Content-MD5 header for requests that
-// require it.
-//func contentMD5(req *http.Request) (err error){
-//	h := md5.New()
-//
-//	// hash the body.  seek back to the first position after reading to reset
-//	// the body for transmission.  copy errors may be assumed to be from the
-//	// body.
-//	_, err = io.Copy(h, req.Body)
-//	if err != nil {
-//		return
-//	}
-//	io.ReadSeeker
-//	_, err = req.Body.Seek(0, 0)
-//	if err != nil {
-//		return
-//	}
-//
-//	// encode the md5 checksum in base64 and set the request header.
-//	sum := h.Sum(nil)
-//	sum64 := make([]byte, base64.StdEncoding.EncodedLen(len(sum)))
-//	base64.StdEncoding.Encode(sum64, sum)
-//	req.Header.Set("Content-MD5", string(sum64))
-//}
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func Bool(v bool) *bool { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func Int(v int) *int { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func String(v string) *string { return &v }
